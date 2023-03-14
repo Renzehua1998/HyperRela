87c938d4b39aa202a1db297226490a084b541866
Zehua Ren <2579734849@qq.com>
2023-03-14 19:04:42 +0800

    initialization

---
+++ others/hypergraph-CNM/dblp.py
@@ -0,0 +1,154 @@
+
+# coding: utf-8
+
+# # Strict and 2-section Hypergraph Clustering
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+import igraph as ig
+from sklearn.metrics import adjusted_rand_score as ARI
+from strictModularity import *
+
+
+# In[2]:
+
+
+def hcut(H,P):
+    s = 0
+    l = 0
+    for i in range(len(H)):
+        l = l + len(H[i])
+        for j in range(len(P)):
+            s = s + sum([x < set(P[j]) for x in H[i] ])
+    return (l-s)/l
+
+
+# In[3]:
+
+
+import pickle
+H, truth, partitions = pickle.load(open('dblp.pkl','rb'))
+PL = partitions[0] ## 2-section Louvain
+PC = partitions[1] ## hypergraph-CNM
+
+
+# In[4]:
+
+
+## make igraph object
+n, m = H_size(H)
+e, w = TwoSecEdges(H,m)
+g = ig.Graph()
+g.add_vertices(n)
+g.add_edges([tuple(x) for x in e])
+g.es["weight"] = w
+g = g.simplify(combine_edges=sum)
+
+
+# In[5]:
+
+
+## uncomment to re-compute the Louvain partition
+#ml = g.community_multilevel(weights="weight")
+#PL = [x for x in ml]
+
+
+# In[6]:
+
+
+## Table1 with Louvain
+print('Table 1 with Louvain')
+print('H-modularity ',modularityH(H,PL))
+print('G-modularity ',modularityG(H,PL))
+print('hcut ',hcut(H,PL))
+print('number of parts ',len(PL))
+
+
+# In[7]:
+
+
+## uncomment to re-compute the hyper-CNM partition -- NB: this is slow!
+#
+#qC, PC = cnmAlgo(H, verbose=True)
+
+
+# In[8]:
+
+
+## Table1 with hypergraph-CNM
+print('Table 1 with hypergraph-CNM')
+print('H-modularity ',modularityH(H,PC))
+print('G-modularity ',modularityG(H,PC))
+print('hcut ',hcut(H,PC))
+print('number of parts ',len(PC))
+
+
+# In[9]:
+
+
+def member(P):
+    s = sum([len(x) for x in P])
+    M = [-1]*s
+    for i in range(len(P)):
+        for j in list(P[i]):
+            M[j]=i
+    return M
+
+def edgeLabels(g, gcomm):
+    x = [(gcomm[x.tuple[0]]==gcomm[x.tuple[1]]) for x in g.es]
+    return x
+def AGRI(g, u, v):
+    bu = edgeLabels(g, u)
+    bv = edgeLabels(g, v)
+    su = sum(bu)
+    sv = sum(bv)
+    suv = sum(np.array(bu)*np.array(bv))
+    m = len(bu)
+    return((suv - su*sv/m) / (0.5*(su+sv) - su*sv/m))
+
+## cluster similarity
+print('Partition similarity measures')
+print('ARI ',ARI(member(PL),member(PC)))
+print('AGRI ',AGRI(g,member(PL),member(PC)))
+
+
+# In[10]:
+
+
+def hcutPlus(H,P):
+    hc = [-1]*len(H)
+    S = 0
+    L = 0
+    for i in range(len(H)):
+        l = len(H[i])
+        L = L + l
+        s = 0
+        for j in range(len(P)):            
+            s = s + sum([x < set(P[j]) for x in H[i] ])
+        S = S + s
+        if l>0:
+            hc[i] = (l-s)/l
+    return hc
+
+
+# In[11]:
+
+
+## Louvain partition
+hc = hcutPlus(H,PL)
+print('With Louvain partition:')
+for i in [2,3,4]:
+    print('prop. of edges of size',i,'cut:',hc[i])
+
+
+# In[12]:
+
+
+## hyper-CNM partition
+hc = hcutPlus(H,PC)
+print('With hyper-CNM partition:')
+for i in [2,3,4]:
+    print('prop. of edges of size',i,'cut:',hc[i])
+
---
+++ others/hypergraph-CNM/hypergraph.py
@@ -0,0 +1,133 @@
+
+# coding: utf-8
+
+# # Strict and 2-section Hypergraph Clustering
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from sklearn.metrics import adjusted_rand_score as Rand
+import igraph as ig
+from scipy import stats
+
+## All hypergraph functions are here
+from strictModularity import *
+
+np.random.seed(0)
+import random
+random.seed(0)
+# In[2]:
+
+
+## proportion of hyperedges cuted w.r.t. partition P
+def hcut(H,P):
+    s = 0
+    l = 0
+    for i in range(len(H)):
+        l = l + len(H[i])
+        for j in range(len(P)):
+            s = s + sum([x < set(P[j]) for x in H[i] ])
+    return (l-s)/l
+
+
+# In[3]:
+
+
+## number of hyperedge of each type
+pointsPerLine = 30
+numOutliers = 60
+REP = 100
+nl = [0]*pointsPerLine + [1]*pointsPerLine + [2]*pointsPerLine + [3]*numOutliers
+
+## is this edge from the same line?
+def lineEdge(e):
+    s = set([nl[i] for i in e])
+    return (len(s)==1 and 3 not in s)
+
+L = []
+mu = 0.33 ## proportion of noisy edges to keep
+
+for z in [0.5,2,1]: ## ratios of 3-edges and 4-edges
+
+    fn = './hypergraph_3uniform.csv'
+    x3 = np.loadtxt(fn, delimiter=',', dtype='int')
+    fn = './hypergraph_4uniform.csv'
+    x4 = np.loadtxt(fn, delimiter=',', dtype='int')
+    ## Downsample
+    p3 = .1 * z
+    base3 = [set(i) for i in x3 if np.random.random()<p3]
+    p4 = .035 / z
+    base4 = [set(i) for i in x4 if np.random.random()<p4]
+    base = base3+base4
+    
+    for rep in range(REP):
+        ## sample down False cases to get mu
+        x = [lineEdge(i) for i in base]
+        t = x.count(True)
+        f = x.count(False)
+        p = mu*t/((1-mu)*f)
+        h = [base[i] for i in range(len(base)) if x[i] or np.random.sample()<p]
+        H = list2H(h)
+        ## 2-section edges and weights
+        n, m = H_size(H)
+        e, w = TwoSecEdges(H,m)
+        ## make weighted igraph object
+        g = ig.Graph()
+        g.add_vertices(n)
+        g.add_edges([tuple(x) for x in e])
+        g.es["weight"] = w
+        g = g.simplify(combine_edges=sum)
+        ## Louvain partition
+        ml = g.community_multilevel(weights="weight")
+        P = [x for x in ml]
+        l = [len(x) for x in H]
+        ## modularities and hcut
+        L.append([l[3]/(l[3]+l[4]),modularityH(H,P),modularityG(H,P),hcut(H,P)])
+      
+
+
+# In[5]:
+
+
+D = pd.DataFrame(L, columns=['ratio_3','qH','qG','hcut'])
+x = D['ratio_3']>.6
+y = D['ratio_3']>.4
+D['regime'] = [int(x[i])+int(y[i]) for i in range(len(x))]
+
+
+# In[9]:
+
+
+X = D[D['regime']==0]
+plt.plot(X['hcut'],X['qG'],'*r',label='majority of 4-edges')
+X = D[D['regime']==2]
+plt.plot(X['hcut'],X['qG'],'*b',label='majority of 3-edges')
+X = D[D['regime']==1]
+plt.plot(X['hcut'],X['qG'],'*g',label='balanced')
+plt.legend();
+plt.xlabel('Hcut value',fontsize=14)
+plt.ylabel('Graph modularity', fontsize=14)
+slope, intercept, r, p, stderr = stats.linregress(X['hcut'],X['qG'])
+print('Balanced case -- Slope:', slope,' R_squared:',r*r)
+plt.savefig('lines_qG.eps')
+
+
+# In[10]:
+
+
+X = D[D['regime']==0]
+plt.plot(X['hcut'],X['qH'],'*r',label='majority of 4-edges')
+X = D[D['regime']==2]
+plt.plot(X['hcut'],X['qH'],'*b',label='majority of 3-edges')
+X = D[D['regime']==1]
+plt.plot(X['hcut'],X['qH'],'*g',label='balanced')
+plt.legend()
+plt.xlabel('Hcut value',fontsize=14)
+plt.ylabel('Hypergraph modularity', fontsize=14)
+slope, intercept, r, p, stderr = stats.linregress(X['hcut'],X['qH'])
+print('Balanced case -- Slope:', slope,' R_squared:',r*r)
+plt.savefig('lines_qH.eps')
+
